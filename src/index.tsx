496d79858547ecc31ea53204eb8807fbf807fbe7
XueWenpeng <15146529212@163.com>
2022-07-27 11:33:00 +0800

    test: test push

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.less';
 
+console.log(1);
 const root = createRoot(document.querySelector('#root') || document.body);
 
 root.render(<div>hahah</div>);
